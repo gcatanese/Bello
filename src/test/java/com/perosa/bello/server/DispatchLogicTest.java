417e00913bd8067d07efa385aeea2e782d6dbb91
perosa <beppe.catanese@gmail.com>
2019-09-17 13:01:14 +0200

    Increase coverage

--- src/test/java/com/perosa/bello/server/DispatchLogicTest.java
+++ src/test/java/com/perosa/bello/server/DispatchLogicTest.java
@@ -3,10 +3,14 @@ package com.perosa.bello.server;
 import com.perosa.bello.core.balancer.Balancer;
 import io.undertow.server.HttpHandler;
 import io.undertow.server.HttpServerExchange;
+import io.undertow.util.HeaderMap;
+import io.undertow.util.HttpString;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
@@ -58,6 +62,20 @@ class DispatchLogicTest {
         assertNotNull(request);
         assertEquals("localhost", request.getHost());
         assertEquals(null, request.getPayload());
+    }
+
+    @Test
+    void extractHeaders() {
+        HeaderMap headerMap = new HeaderMap();
+        headerMap.add(new HttpString("host"), "localhost");
+        headerMap.add(new HttpString("authorization"), "bldfsdf$%45");
+
+        when(exchange.getRequestHeaders()).thenReturn(headerMap);
+
+        Map<String, String> map = new DispatchLogic(balancer).extractHeaders(exchange);
+
+        assertNotNull(map);
+        assertEquals(2, map.size());
     }
 
 }
