d56b862fdfd91117f7038aa59fcc08822076c939
perosa <beppe.catanese@gmail.com>
2019-10-16 20:52:43 +0200

    Add toString

--- src/main/java/com/perosa/bello/core/resource/event/Event.java
+++ src/main/java/com/perosa/bello/core/resource/event/Event.java
@@ -1,4 +1,38 @@
 package com.perosa.bello.core.resource.event;
 
+import com.perosa.bello.server.InRequest;
+
 public class Event {
+
+    private InRequest request;
+    private String target;
+
+    public Event(InRequest request, String target) {
+        this.request = request;
+        this.target = target;
+    }
+
+    public InRequest getRequest() {
+        return request;
+    }
+
+    public void setRequest(InRequest request) {
+        this.request = request;
+    }
+
+    public String getTarget() {
+        return target;
+    }
+
+    public void setTarget(String target) {
+        this.target = target;
+    }
+
+    @Override
+    public String toString() {
+        return "Event[" +
+                "target:" + target +
+                "," + request +
+                "]";
+    }
 }
