c5ea36eac8504cc0907ae264b1d92ffdd6ed1ab1
perosa <beppe.catanese@gmail.com>
2019-10-16 17:34:33 +0200

    Introduce mbassador for publishing events

--- src/main/java/com/perosa/bello/core/resource/event/EventListener.java
+++ src/main/java/com/perosa/bello/core/resource/event/EventListener.java
@@ -1,5 +1,6 @@
 package com.perosa.bello.core.resource.event;
 
+import com.perosa.bello.core.resource.event.metrics.PrometheusEventHandler;
 import net.engio.mbassy.listener.Handler;
 import net.engio.mbassy.listener.Invoke;
 import net.engio.mbassy.listener.Listener;
@@ -14,6 +15,8 @@ public class EventListener {
     @Handler(delivery = Invoke.Asynchronously)
     public void handle(Event event){
         LOGGER.debug("handling " + event);
+
+        new PrometheusEventHandler().process(event);
 
     }
 }
---
+++ src/main/java/com/perosa/bello/core/resource/event/metrics/PrometheusEventHandler.java
@@ -0,0 +1,10 @@
+package com.perosa.bello.core.resource.event.metrics;
+
+import com.perosa.bello.core.resource.event.Event;
+
+public class PrometheusEventHandler {
+
+    public void process(Event event) {
+
+    }
+}
