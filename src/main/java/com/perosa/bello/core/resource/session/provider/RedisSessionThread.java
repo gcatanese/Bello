472d0cd58495cd657733f28760c6e8e86b12462a
perosa <beppe.catanese@gmail.com>
2019-10-18 10:38:59 +0200

    Remove expired sessions

---
+++ src/main/java/com/perosa/bello/core/resource/session/provider/RedisSessionThread.java
@@ -0,0 +1,43 @@
+package com.perosa.bello.core.resource.session.provider;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class RedisSessionThread {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(RedisSessionThread.class);
+
+    private static final long MAX_DURATION = 15;
+    private RedisSessionCache cache;
+
+    public RedisSessionThread(RedisSessionCache cache) {
+        this.cache = cache;
+    }
+
+    public void start() {
+
+        Timer timer = new Timer("redisessionThread");
+        final long INTERVAL = 1000L * 60 * 5;
+
+        TimerTask task = new TimerTask() {
+            public void run() {
+                cleanUp();
+            }
+        };
+
+        timer.schedule(task, 5000L, INTERVAL);
+
+    }
+
+    void cleanUp() {
+        cache.getMap().entrySet().stream()
+                .filter(e -> e.getValue().getDate().isBefore(LocalDateTime.now().minusMinutes(MAX_DURATION)))
+                .forEach(e -> cache.remove(e.getKey()));
+    }
+}
